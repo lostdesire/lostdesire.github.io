2b504e45fdf3163129689afec4e47d073462993a
lostdesire <lopkawqs@gmail.com>
2023-11-06 23:47:41 +0900

    sitemap & robots

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1699281080';
+const cacheName = 'chirpy-1699281801';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
