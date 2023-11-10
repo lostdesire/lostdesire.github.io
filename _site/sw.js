6f3604b3a4d5d1f018c456f1798b7f37348b8c9b
lostdesire <lopkawqs@gmail.com>
2023-11-10 15:59:25 +0900

    Setting posting order

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1699594187';
+const cacheName = 'chirpy-1699599516';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
