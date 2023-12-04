914bfe5223150356ce85607625391d934a82d729
lostdesire <lopkawqs@gmail.com>
2023-12-04 21:38:29 +0900

    Day21

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1701424310';
+const cacheName = 'chirpy-1701693492';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
