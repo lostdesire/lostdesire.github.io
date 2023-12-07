f9abaf527b94f8e121eb8ba8b2c488b0c203e686
lostdesire <lopkawqs@gmail.com>
2023-12-07 22:55:35 +0900

    Mathjax guide

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1701951971';
+const cacheName = 'chirpy-1701957285';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
