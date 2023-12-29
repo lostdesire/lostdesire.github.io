db636294ca7228957b540080402df86ac93d3c70
lostdesire <lopkawqs@gmail.com>
2023-12-29 20:00:58 +0900

    Day40

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1702548188';
+const cacheName = 'chirpy-1703821942';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
