1171880839fc33d177ad06fe2327443516fb39e6
lostdesire <lopkawqs@gmail.com>
2024-01-03 15:30:18 +0900

    Update jekyll

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1704263227';
+const cacheName = 'chirpy-1704263399';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
